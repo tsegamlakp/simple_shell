f7397ded948ec8203d098d7ab8e7cd53a5a17e75
tsegamlakp <99799402+tsegamlakp@users.noreply.github.com>
2022-05-19 01:03:35 +0300

    done

--- main.c
+++ main.c
@@ -1,89 +1,28 @@
-#include "shell.h"
+#include "hsh.h"
 
 /**
- * main - Initation funtion
- * @argc: number of the arguments
- * @argv: arguments
- * @env: enviroments
- * Return: success full 1 or fail -1
- **/
-int main(int argc, char **argv, char **env)
-{
-	var_t vars = {0, 0, 0, NULL, NULL, NULL};
-	size_t size = 0;
-	int varget = 0;
-
-	(void)argc;
-	vars.nameShell = argv[0];
-
-	if (isatty(STDIN_FILENO))
-		write(1, "$ ", 3);
-	signal(SIGINT, sighandler);
-	while (varget != EOF)
-	{
-		varget = getline(&vars.comand, &size, stdin);
-		if (varget == 1)
-		{
-			if (isatty(STDIN_FILENO))
-				write(1, "$ ", 3);
-			continue;
-		}
-		if (varget == -1)
-			break;
-		vars.tk_i++;
-		if (tokens(&vars, env) == 0)
-		{
-			execute(&vars, env);
-		}
-		if (isatty(STDIN_FILENO))
-			write(1, "$ ", 3);
-		free(vars.comand);
-		free(vars.tokens);
-		vars.comand = NULL;
-		size = 0;
-	}
-
-	free(vars.comand);
-	return (0);
-}
-
-/**
- * tokens - Initation funtion
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * @vars: point
- * @env: point
- *
- * Return: success full
+ * Return: 0 on success, 1 on error
  */
-int tokens(var_t *vars, char **env)
+int main(int ac, char **av)
 {
-	char delimitador[] = ",;\n\t ";
-	int i = 0;
+	info_t info[] = { INFO_INIT };
 
-	vars->tokens = malloc(sizeof(char *) * 1024);
-	if (vars->tokens == NULL)
-		exit(0);
-	vars->tokens[i] = strtok(vars->comand, delimitador);
-	while (vars->tokens[i]  != NULL)
+	if (ac == 2)
 	{
-	i++;
-	vars->tokens[i] = strtok(NULL, delimitador);
-	vars->contk = i;
+		info->readfd = open_file(info, av[1], 0);
+		if (info->readfd == -1)
+		{
+			free_info(info, 1);
+			exit(info->err_num);
+		}
 	}
-
-	if (vars->tokens[0] == NULL)
-		return (1);
-	return (isCommand(vars, env));
-}
-
-/**
- * sighandler - Initation funtion varible
- *
- * @signum: int argument
- *
- */
-void sighandler(int signum)
-{
-	(void)signum;
-	write(1, "\n$ ", 3);
+	populate_env_list(info);
+	read_history(info);
+	read_startup_file(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
